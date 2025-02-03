f98c8384c4d23d9d85f0e08b824b91fd14c52600
LabRat142 <141347648+LabRat142@users.noreply.github.com>
2025-02-03 13:06:42 +0100

    products model, stores config, scraper.py file

---
+++ config/stores.php
@@ -0,0 +1,7 @@
+return [
+    'stores' => [
+        ['name' => 'Anhoch', 'search_url' => 'https://www.anhoch.com/products?query='],
+        ['name' => 'Setec', 'search_url' => 'https://setec.mk/index.php?route=product/search&search='],
+        ['name' => 'Technomarket', 'search_url' => 'https://www.tehnomarket.com.mk/products/search?search='],
+    ],
+];
